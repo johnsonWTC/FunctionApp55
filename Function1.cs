f94e0125499b3acf06c5e14fa18c835f0dc3e532
Johnson Dubula <jdubula@iqbusiness.net>
2021-11-18 18:00:00 +0300

    Functions

--- Function1.cs
+++ Function1.cs
@@ -23,8 +23,7 @@ namespace FunctionApp55
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
+ 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
