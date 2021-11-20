20bb4a1a3b51b34dd2c747e3696a43b3f0084da2
Johnson Dubula <jdubula@iqbusiness.net>
2021-11-20 18:00:00 +0300

    Functions

--- Function1.cs
+++ Function1.cs
@@ -14,7 +14,7 @@ namespace FunctionApp55
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "")] HttpRequest req,
             ILogger log)
         {
             return new OkObjectResult("");
